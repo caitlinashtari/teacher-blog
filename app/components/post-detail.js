1cee40f0cdbbd4ae58e68199103c994e5458089f
Caitlin Ashtari <caitlinashtari@gmail.com>
2017-01-12 10:52:11 -0800

    add post-detail component with delete functionality.

---
+++ app/components/post-detail.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  actions: {
+    delete(post){
+      if(confirm('Are you sure?')) {
+        this.sendAction('destroyPost', post);
+      }
+    }
+  }
+});
