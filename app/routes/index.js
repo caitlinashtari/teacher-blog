30e7661f98767d4133cffd4951eea12a09d8bbca
Caitlin Ashtari <caitlinashtari@gmail.com>
2017-01-12 11:52:46 -0800

    add update-post component with update functionality and refactor code.

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,18 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll('post')
+    return this.store.findAll('post');
   },
   actions: {
-    destroyPost(post) {
-      post.destroyRecord();
-      this.trasitionTo('index');
-    },
-
     savePost3(params){
       var newPost = this.store.createRecord('post', params);
       newPost.save();
-      this.trasitionTo('index');
+      this.transitionTo('index');
     }
   }
 });
